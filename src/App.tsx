98b80fcbd69485f10fbe728134758c52109a1f63
cyrusumali <umalicyrus04171998@gmail.com>
2026-05-07 15:35:20 +0800

    Name correction

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
 import { Toaster } from "sonner";
-import supabase from "./lib/supabase";
 
 export default function App() {
   return (
